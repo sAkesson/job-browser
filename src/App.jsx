51ba27fc6628b7ac073d9adc461b54f1e431014c
sandraevelina <sandra@sandraevelina.com>
2022-02-20 22:12:49 +0100

    Add error handling

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,68 @@ import './App.css';
 
 const App = () => {
   const [jobAds, setJobAdds] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(true);
 
   const getAllJobAds = async () => {
-    return fetch('https://feed.jobylon.com/feeds/7d7e6fd12c614aa5af3624b06f7a74b8/?format=json')
-      .then((response) => response.json())
-      .catch((error) => console.warn(error));
+    return fetch(
+      'https://feed.jobylon.com/feeds/7d7e6fd12c614aa5af3624b06f7a74b8/?format=json'
+    ).then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      }
+
+      throw new Error('Something went wrong, reload the page to try again');
+    });
   };
 
   useEffect(async () => {
-    const data = await getAllJobAds();
+    try {
+      const data = await getAllJobAds();
 
-    setJobAdds(data);
+      setJobAdds(data);
+      setLoading(false);
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
   }, []);
 
   return (
     <div className="container">
-      {jobAds.map((post) => {
-        const {
-          id,
-          title,
-          company,
-          locations,
-          function: jobFunction,
-          experience,
-          employment_type,
-          descr,
-          urls,
-        } = post;
-        const location = locations.map((site) => site?.location?.text ?? '').filter((site) => site);
-        const tags = [...location, experience, employment_type];
+      {!errorMessage.length &&
+        !loading &&
+        jobAds.map((post) => {
+          const {
+            id,
+            title,
+            company,
+            locations,
+            function: jobFunction,
+            experience,
+            employment_type,
+            descr,
+            urls,
+          } = post;
+          const location = locations
+            .map((site) => site?.location?.text ?? '')
+            .filter((site) => site);
+          const tags = [...location, experience, employment_type];
 
-        return (
-          <ListItem
-            key={id}
-            title={title}
-            subTitle={`@ ${company.name}`}
-            metadata={`${jobFunction}, ${experience}`}
-            body={descr}
-            tags={tags}
-            action={() => window.open(urls.ad, '_blank')}
-          />
-        );
-      })}
+          return (
+            <ListItem
+              key={id}
+              title={title}
+              subTitle={`@ ${company.name}`}
+              metadata={`${jobFunction}, ${experience}`}
+              body={descr}
+              tags={tags}
+              action={() => window.open(urls.ad, '_blank')}
+            />
+          );
+        })}
+      {errorMessage && !loading && <p>{errorMessage}</p>}
+      {loading && <p>Loading content... </p>}
     </div>
   );
 };
